c3495f358cd00978e6c22753ed0c3af55d8a378d
rmaini10 <rmaini.10@gmail.com>
2019-08-13 18:40:00 +0530

    updated contact-2.php

---
+++ After Editing/aStar/contact-2.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="aStar Fashion Template Project">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.3/bootstrap.css">
+    <link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+    <link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.2.1/animate.css">
+    <link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+    <link rel="stylesheet" type="text/css" href="styles/responsive.css">
+    <link rel="stylesheet" href="styles/index_cardlayout.css">
+    <title>edit4</title>
+</head>
+
+<body>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="styles/bootstrap-4.1.3/popper.js"></script>
+    <script src="styles/bootstrap-4.1.3/bootstrap.min.js"></script>
+    <script src="plugins/greensock/TweenMax.min.js"></script>
+    <script src="plugins/greensock/TimelineMax.min.js"></script>
+    <script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+    <script src="plugins/greensock/animation.gsap.min.js"></script>
+    <script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+    <script src="plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+    <script src="plugins/easing/easing.js"></script>
+    <script src="plugins/parallax-js-master/parallax.min.js"></script>
+    <script src="plugins/Isotope/isotope.pkgd.min.js"></script>
+    <script src="plugins/Isotope/fitcolumns.js"></script>
+    <script src="js/custom.js"></script>
+
+    <div class="super_container">
+        <?php 
+            include 'header.php';
+        ?>
+        <br>
+        <br>
+        <contact>
+            
+            <div style="background-color:light-grey;" class="container-fluid">
+            
+                <div class="row">
+                    <div style="text-align:center; color:black;" class="col-md-12 heading-title"> <h1>Get in touch with us</h1></div>
+                </div> <br>
+                <div class="row">
+                    <div class="col-md-2"></div>
+                    <div class="col-md-4">
+                        <div class="contact-box">
+                            <ul class="list-inline">
+                                <li>
+                                    <div class="contact">
+                                        <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                        <p> DieSachbearbeiter Choriner Straße 49 <br> 10435 Berlin </p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="contact">
+                                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                                        <p> Info@example.com <br> example@yourcompany.com </p>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="contact">
+                                        <i class="fa fa-mobile" aria-hidden="true"></i>
+                                        <p> +91 9876543210 <br> +91 5689452158 </p>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div> <br> <br>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-box-inner">
+                            <form action="" method="">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" for="" name="" value="" placeholder="Your name">
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control" for="" name="" value="" placeholder="Your email">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" for="" name="" value="" placeholder="Your number">
+                                </div>
+                                <div class="form-group">
+                                    <textarea rows="4" cols="50" class="form-control" placeholder="Describe yourself here..."></textarea>
+                                </div>
+                                <button class="btn contactcustom-button"> Submit Now </button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="col-md-2"></div>
+
+
+
+                </div>
+                <br> <br>
+                <div class="row">
+                    <div style="margin:0; padding:0; max-width:1920px;" class="col-md-12">
+                        <div class="map-box">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235526.90924712367!2d75.7237618214095!3d22.724228429904368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3962fcad1b410ddb%3A0x96ec4da356240f4!2sIndore%2C+Madhya+Pradesh!5e0!3m2!1sen!2sin!4v1564059499022!5m2!1sen!2sin" width="100%" height="450" frameborder="0" style="padding-left:20px; padding-right:20px; border: 0;" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </contact>
+        <?php
+            include 'footer.php';
+        ?>
+
+    </div>
+</body>
+
+</html>
