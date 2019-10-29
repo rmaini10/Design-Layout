a437debde8e9a5a48ffa9e0e749ba9a97dd1ae0b
Rakshit <rmaini.10@gmail.com>
2019-10-29 06:04:51 -0400

    Add files via upload

--- After Editing/aStar/js/whatsapp.js
+++ After Editing/aStar/js/whatsapp.js
@@ -1,5 +1,12 @@
 function order(){
-        var x= $("#getpro").parents("#pro").attr("productid");
+        //var x= $("#getpro").parents("#pro").attr("productid");
+        var x= document.getElementById("btn_whatsapp").parentNode.getAttribute("productid");
         url= 'https://api.whatsapp.com/send?phone=' + 919999089262 + '&text=%20' + x;
         location.href =url;
+        
     }
+function whatsapp(){
+    var x = document.getElementById("whatsapp").parentNode.children[0].children[0].children[0].innerHTML;
+    url= 'https://api.whatsapp.com/send?phone=' + 919999089262 + '&text=%20' + x;
+        location.href =url;
+}
